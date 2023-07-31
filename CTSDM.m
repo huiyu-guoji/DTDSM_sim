b8aedea481fcd86dcff8535f73812f1b2e54a97a
huiyu-guoji <13663585559@163.com>
2023-07-31 16:31:51 +0800

    fix the dBFS

--- CTSDM.m
+++ CTSDM.m
@@ -1,19 +1,19 @@
 %2阶，512osr，理论最高能做到enob=20.89
 %参数设置
 
-osr=1024;
+osr=2048;
 fb=0;
-fs=osr*2*2e4;
+fs=osr*2*2e3;
 order=2;
 opt=1;
 N=1;
 H_inf=1.5;
 
-%假设实际的Vref=3
+
 vref= 2^N;
 VDD=1.5/1.2*vref;
 
-fin=21/2^16;%按fs归一化相干采样
+fin=37/2^16;%按fs归一化相干采样
 
 %打印参数
 s0=sprintf('**Simulation Parameters **');
@@ -40,19 +40,21 @@ disp(s7);
 %理想模型能达到的指标
 ntf=synthesizeNTF(order,osr,opt,H_inf,fb);
 ntf.Ts=1/fs;
-% [snr,amp] = simulateSNR(ntf,osr);
-% plot(amp,snr,'-*');
-% % %bode(ntf);
-% xlabel('Input Amplitude/dBFS');
-% ylabel('SNDR/dB');
+
+[snr,amp] = simulateSNR(ntf,osr,[],[],[],[],17);
+plot(amp,snr,'-*');
+% bode(ntf);
+xlabel('Input Amplitude/dBFS');
+ylabel('SNDR/dB');
 
 
 [a,g,b,c] = realizeNTF(ntf,'CIFF');
 %状态转移
 ABCD=stuffABCD(a,g,b,c,'CIFF');
 %积分器保证增益的最大摆幅
-[ABCDs,vinmax]=scaleABCD(ABCD,2^N,fin,0.6*VDD,vref);
+[ABCDs,umax]=scaleABCD(ABCD,2^N,fin,VDD,vref);
+%[ABCDs,umax,S]=scaleABCD(ABCD,2,0,1,7,umax,1e5,10);
 %实际的保证积分器和量化器摆幅的vinmax
-vinmax=vinmax*1.5/VDD;
+vinmax=umax*1.5/VDD;
 %实际电路对应
 [a1,g1,b1,c1]=mapABCD(ABCDs,'CIFF');
--- para_sim_2b.m
+++ para_sim_2b.m
@@ -5,15 +5,16 @@ clear all
 CTSDM();
 n=0;
 modulename='SimPara_DTDSM_2b_2or_512osr';
-%modulename='un_120dB_DTDSM_1b_2or_1024osr';
+%modulename='un_110dB_DTDSM_1b_2or_1024osr';
 load_system(modulename);
  %for  cap=-15 :0.5:-10
  %for  deltadB=-12 :0.2:-6
 
  %for  AdB=20:5:140    %需要改变的参数
    
- %for  A=0.00001:0.1:3.3
- for A=-15:0.5:-10
+ for  A=0.00001:0.05:1.6
+ 
+ %for A=-15:0.5:-10
  %for A=6:0.5:12     
  %for A=1:0.5:10   
     n=n+1;
@@ -21,7 +22,7 @@ load_system(modulename);
     disp(s1);
     fname = n;
     
-    c=1*10^A; 
+    %c=1*10^A; 
     % sr=1*10^A;
     %c=cap*10^(-12);
     %delta=10^(deltadB);
@@ -37,18 +38,18 @@ load_system(modulename);
     
     %set_param('un_DTDSM_1b_2or_512osr/Sine Wave1','Amplitude',num2str(k,'%.4f'));
     set_param('SimPara_DTDSM_2b_2or_512osr/Power Spectral Density','savename',num2str(fname)); 
-    %set_param('un_120dB_DTDSM_1b_2or_1024osr/Power Spectral Density','savename',num2str(fname)); 
+%     set_param('un_110dB_DTDSM_1b_2or_1024osr/Power Spectral Density','savename',num2str(fname)); 
     simOut=sim(modulename);
     load(num2str(fname));
     result(n,2)=SNR_dB;
 
-    %result(n,1)=20*log10(A/1.5);
+    result(n,1)=20*log10(A/1.5);
 %     result(n,1)=sr;
     %result(n,1)=delta;
 %     result(n,1)=GBW;
 %    result(n,1)=AdB;
 %      result(n,1)=A;
-       result(n,1)=c;   
+%       result(n,1)=c;   
     
     s3=sprintf("**A = %d  simulation result: SNR_dB %f **",A,result(n,2));
     disp(s3);
@@ -56,18 +57,18 @@ load_system(modulename);
  end
     delete *.mat
    plot(result(:,1),result(:,2),'-*');
-    xlabel("Sample Capacitance[F]");
+%     xlabel("Sample Capacitance[F]");
 %    xlabel("SR[V/s]");
 %    xlabel('Noise[V]');
-%    xlabel("Input Amplitude[dBFS]");
+   xlabel("Input Amplitude[dBFS]");
 %    xlabel("OP-amp Open Loop Gain[dB]");
 %    xlabel('GBW[Hz]');
 %    xlabel("Saturation[V]");
  
-x_ticks = 10.^(-15 :1: -10);
-   set(gca, 'XScale', 'log');  % Set X-axis to logarithmic scale
-   set(gca, 'XTick', x_ticks);  % Set X-axis ticks to 2, 4, 8, 16, and so on
-   set(gca, 'XTickLabel', x_ticks);  % Set X-axis tick labels to the corresponding values
+% x_ticks = 10.^(-15 :1: -10);
+%    set(gca, 'XScale', 'log');  % Set X-axis to logarithmic scale
+%    set(gca, 'XTick', x_ticks);  % Set X-axis ticks to 2, 4, 8, 16, and so on
+%    set(gca, 'XTickLabel', x_ticks);  % Set X-axis tick labels to the corresponding values
 
    ylabel("SQNR[dB]");
    s2=sprintf("**simulation end **\n** %d times in total**",n);
